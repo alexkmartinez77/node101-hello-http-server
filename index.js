19b4220521351783cf129119184d181e20591a54
Alex Martinez <alexkmartinez77@gmail.com>
2021-10-22 10:49:28 -0700

    Adds code to return header (200), rul log and hello world.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// write your code here
+const http = require('http');
+
+const server = http.createServer(function(req, res){
+  //returns 200 header
+  res.writeHead(200 , {'Content-Type': 'text/plain'});
+  //Log path
+  console.log('Request URL:' + req.url);
+  //returns message
+  res.end('Hello World');
+
+});
+
+server.listen(8080, 'localhost', null, function() {console.log('Server is listening on 8080.')})
